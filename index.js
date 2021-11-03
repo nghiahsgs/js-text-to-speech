fb49bd4ec6580149651717b27e79e3fc1059253e
nghiahsgs <nghiahsgs@gmail.com>
2021-11-03 11:34:23 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+function speak(message) {
+  var msg = new SpeechSynthesisUtterance(message)
+  var voices = window.speechSynthesis.getVoices()
+  msg.voice = voices[0]
+  window.speechSynthesis.speak(msg)
+}
+speak("I love you")
